d5372f68facad7d081c107afa8991eabac4038d3
gallag80 <gallag80@purdue.edu>
2022-08-26 08:29:25 -0400

    CGT 215: Lab 1

--- CGT 215Lab 1/CGT 215Lab 1.cpp
+++ CGT 215Lab 1/CGT 215Lab 1.cpp
@@ -5,7 +5,7 @@
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::cout << "Matthew Gallagher is good at basketball\n";
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
